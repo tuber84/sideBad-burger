ed6696d3b5b28ac1cc9469f9ccb05be4f41c7dd0
tuber84 <tuberkulum@gmail.com>
2022-06-05 15:50:07 +0400

    мобильная версия:ниспадащее меню с помощью js+поворот "стрлеки"

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ const isMobile = {
 
 if (isMobile.any()) {
   document.body.classList.add(`_touch`);
+  let menuArrows = document.querySelectorAll(`.menu__arrow`);
+//   console.log(menuArrows.length);
+  if (menuArrows.length > 0) {
+    for (let i = 0; i < menuArrows.length; i++) {
+      const menuArrow = menuArrows[i];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle(`_active`);
+      });
+    }
+  }
 } else {
   document.body.classList.add(`_pc`);
 }
