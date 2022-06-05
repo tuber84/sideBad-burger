888cc606fbf43c2dcc34695ddb501be260acad72
tuber84 <tuberkulum@gmail.com>
2022-06-05 11:08:55 +0400

    меню для ps появляющееся

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-'use strict'
+"use strict";
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return isMobile.Android() || isMobile.Windows();
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add(`_touch`);
+} else {
+  document.body.classList.add(`_pc`);
+}
