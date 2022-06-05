c2ea185f9be85ef438c0b5533f3dd60c53ca051f
tuber84 <tuberkulum@gmail.com>
2022-06-05 16:56:50 +0400

    плавная перемотка к определенной части страницы на js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const isMobile = {
 if (isMobile.any()) {
   document.body.classList.add(`_touch`);
   let menuArrows = document.querySelectorAll(`.menu__arrow`);
-//   console.log(menuArrows.length);
+  //   console.log(menuArrows.length);
   if (menuArrows.length > 0) {
     for (let i = 0; i < menuArrows.length; i++) {
       const menuArrow = menuArrows[i];
@@ -27,3 +27,35 @@ if (isMobile.any()) {
 } else {
   document.body.classList.add(`_pc`);
 }
+
+//прокрутка при клике к определенному месту страницы
+
+const menuLinks = document.querySelectorAll(`.menu__link[data-goto]`);
+if (menuLinks.length > 0) {
+  for (let menuLink of menuLinks) {
+    menuLink.addEventListener(`click`, onMenuClick);
+  }
+}
+function onMenuClick(e) {
+  const menuLink = e.target;
+  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    const gotoBlock = document.querySelector(menuLink.dataset.goto);
+    const gotoBlockValue =
+      gotoBlock.getBoundingClientRect().top +
+      scrollY -
+      document.querySelector(`header`).offsetHeight;
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth",
+    });
+    e.preventDefault();
+  }
+}
+
+// console.log(menuLinks);
+
+// for (const menuLink of menuLinks) {
+//     console.log(menuLink);
+
+// }
